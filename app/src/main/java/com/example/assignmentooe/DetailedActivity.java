8234a657dd54527028e3c47e76a81a990dceea1a
Aton, Shean Danes <sa372@students.waikato.ac.nz>
2022-08-15 21:18:53 +1200

    Has added a new activity and provided its basic layout.

--- app/src/main/java/com/example/assignmentooe/DetailedActivity.java
+++ app/src/main/java/com/example/assignmentooe/DetailedActivity.java
@@ -1,16 +1,14 @@
 package com.example.assignmentooe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detailed);
     }
-
-
-
 }
