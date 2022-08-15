ce226af78f827d734727ecc33084786ef7604f9b
Aton, Shean Danes <sa372@students.waikato.ac.nz>
2022-08-16 11:39:21 +1200

    Toast messages has added at the same time the onlick for the landscape has also been implemented.

--- app/src/main/java/com/example/assignmentooe/MainActivity.java
+++ app/src/main/java/com/example/assignmentooe/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.assignmentooe;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View; //Imported View to be able to implement it for the view methods
+import android.widget.Toast; //Importted Toast to be able to implement a toast message
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+        public void cs_subButton(View v){
+            Toast.makeText(this, "The button has been clicked!", Toast.LENGTH_SHORT).show();
+        }
 
+        public void math_subButton(View v){
+            Toast.makeText(this, "The button has been clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        public void design_subButton(View v){
+            Toast.makeText(this, "The button has been clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        public void se_subButton(View v){
+            Toast.makeText(this, "The button has been clicked!", Toast.LENGTH_SHORT).show();
+        }
 
 }
